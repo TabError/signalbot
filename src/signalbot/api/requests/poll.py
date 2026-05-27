b3d7d424959d46b9c5b90b4aedd24d669c8460c5
Gara Dorta <1507774+Gara-Dorta@users.noreply.github.com>
2026-05-27 22:50:04 +0200

    refactor: simplify sent models construction

--- src/signalbot/api/requests/poll.py
+++ src/signalbot/api/requests/poll.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from copy import deepcopy
-
 from signalbot.api.generated import CreatePollRequest
 
 
@@ -12,11 +10,6 @@ class Poll(CreatePollRequest):
     def from_create_poll_request(
         cls, create_poll_request: CreatePollRequest, timestamp: int
     ) -> Poll:
-        create_poll_request = deepcopy(create_poll_request)
-        return cls(
-            allow_multiple_selections=create_poll_request.allow_multiple_selections,
-            answers=create_poll_request.answers,
-            question=create_poll_request.question,
-            recipient=create_poll_request.recipient,
-            timestamp=timestamp,
+        return cls.model_construct(
+            **create_poll_request.model_dump(), timestamp=timestamp
         )
--- src/signalbot/api/requests/send_message.py
+++ src/signalbot/api/requests/send_message.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from copy import deepcopy
-
 from signalbot.api.generated import SendMessageV2
 
 
@@ -16,22 +14,4 @@ class SentMessage(SendMessage):
     def from_send_message(
         cls, send_message: SendMessage, timestamp: int
     ) -> SentMessage:
-        send_message = deepcopy(send_message)
-        return cls(
-            base64_attachments=send_message.base64_attachments,
-            edit_timestamp=send_message.edit_timestamp,
-            link_preview=send_message.link_preview,
-            mentions=send_message.mentions,
-            text=send_message.text,
-            notify_self=send_message.notify_self,
-            number=send_message.number,
-            quote_author=send_message.quote_author,
-            quote_mentions=send_message.quote_mentions,
-            quote_message=send_message.quote_message,
-            quote_timestamp=send_message.quote_timestamp,
-            recipients=send_message.recipients,
-            sticker=send_message.sticker,
-            text_mode=send_message.text_mode,
-            view_once=send_message.view_once,
-            timestamp=timestamp,
-        )
+        return cls.model_construct(**send_message.model_dump(), timestamp=timestamp)
