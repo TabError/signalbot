bf61faa6a73fedd0949a7c1205b257f68c343cb1
Gara Dorta <1507774+Gara-Dorta@users.noreply.github.com>
2026-05-27 22:18:24 +0200

    feat: add update_contact and group to the context

--- src/signalbot/context/context.py
+++ src/signalbot/context/context.py
@@ -4,7 +4,12 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from signalbot.api.receive_messages import ReceivedMessageType
-    from signalbot.api.requests import SendMessage, SentMessage
+    from signalbot.api.requests import (
+        SendMessage,
+        SentMessage,
+        UpdateContactRequest,
+        UpdateGroupRequest,
+    )
     from signalbot.bot import SignalBot
 
 
@@ -40,3 +45,28 @@ class Context:
         [signalbot.SignalBot.stop_typing()](bot.md#signalbot.SignalBot.stop_typing)
          but with the recipient set to the message's recipient."""
         await self.bot.stop_typing(self.message.source_or_group_uuid())
+
+    async def update_contact(
+        self, update_contact_request: UpdateContactRequest
+    ) -> None:
+        """Same as
+        [signalbot.SignalBot.update_contact()](bot.md#signalbot.SignalBot.update_contact)
+         but with the recipient set to the message's recipient."""
+        if self.message.is_group():
+            error_msg = "Cannot update contact for a group message"
+            raise ValueError(error_msg)
+        update_contact_request.recipient = self.message.source_or_group_uuid()
+        await self.bot.update_contact(update_contact_request)
+
+    async def update_group(
+        self,
+        update_group_request: UpdateGroupRequest,
+    ) -> None:
+        """Same as
+        [signalbot.SignalBot.update_group()](bot.md#signalbot.SignalBot.update_group)
+         but with the group id or name set to the message's recipient."""
+        if self.message.is_private():
+            error_msg = "Cannot update group for a private message"
+            raise ValueError(error_msg)
+        update_group_request.group_id_or_name = self.message.source_or_group_uuid()
+        await self.bot.update_group(update_group_request)
